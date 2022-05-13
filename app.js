ba529164b3fc0df2c7d395a711f95664cc13b72c
Dennis Velez <36821370+dvelez1@users.noreply.github.com>
2022-05-12 22:47:44 -0400

    Add GetAll and Post

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const sql = require('mssql/msnodesqlv8')
 var express = require('express');
 var app = express();
+app.use(express.json());
+var bodyParser = require('express');
+var queryString = "";
 
+const { response } = require('express');
+const router = express.Router()
 //msnodesqlv8 module is requiered for Windows Authentication without using Username and Password
 
-
+// ConnectionString
 const pool = new sql.ConnectionPool({
   database: 'Tarea',
   server: 'LENOVO\\SQLEXPRESS',
@@ -13,40 +18,74 @@ const pool = new sql.ConnectionPool({
     trustedConnection: true
   }
 })
-
+// Model
+var tarea = {Id:0, Nombre:"", Estado:""};
 
 
 //GET API
-app.get("/tareas", function(req , res){
-	pool.connect().then(() => {
-        //simple query
-        var queryString = 'select TOP(1) Id,Nombre,Estado  from dbo.Tareas';
-        pool.request().query(queryString, (err, result) => {
-          if(err)
+app.get("/getTareas", function (request, response) {
+  try {
+    pool.connect().then(() => {
+      //simple query
+      queryString = 'select TOP(2) Id,Nombre,Estado  from dbo.Tareas';
+      pool.request().query(queryString, (err, result) => {
+        if (err){
           console.log(err)
-          else {
-              console.log("Resultado",result)
-              res.send(result);
-          }
-          })
+          response.sendStatus(400)
+        }
+        else {
+          console.log("Resultado", result.recordset)
+          response.send(result.recordset);
+        }
       })
+    })
+  } catch (err) {
+    response.status(500)
+    response.send(err.message)
+  }
 });
-// function getTareas() {
-// pool.connect().then(() => {
-//   //simple query
-//   var queryString = 'select TOP(1) Id,Nombre,Estado  from dbo.Tareas';
-//   pool.request().query(queryString, (err, result) => {
-//     if(err)
-//     console.log(err)
-//     else {
-//         console.log("Resultado",result)
-//         res.send(result);
-//     }
-//     })
-// })
-// }
+
+// Get/Id
+
+//Post API
+app.post("/updateTareas", function (request, response) {
+  try {
+
+    tarea.Id = request.body.Id;
+    tarea.Estado = request.body.Estado;
+    tarea.Nombre = request.body.Nombre;
+  
+    pool.connect().then(() => {
+      //simple query
+      queryString = 'Update dbo.Tareas ' + 
+      'SET Nombre = @Nombre, Estado = @Estado' +    
+      ' WHERE id=@Id';
+
+      pool.request()
+      .input("Id", sql.Int, tarea.Id)
+      .input("Nombre", sql.VarChar, tarea.Nombre)
+      .input("Estado", sql.Bit, tarea.Estado)
+      .query(queryString, (err, result) => {
+        if (err){
+          console.log(err)
+          response.sendStatus(400)
+        }
+        else {
+          response.sendStatus(200)
+          //response.status(200).send({message: "Success"})
+        }
+      })
+   
+    })
+
+  } catch (err) {
+    response.status(500)
+    response.send(err.message)
+  }
+});
+
 
 
 var server = app.listen(5000, function () {
-    console.log('Node server is running..');
+  console.log('Node server is running..');
 });
